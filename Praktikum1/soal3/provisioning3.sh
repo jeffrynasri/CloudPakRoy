3c48de560724f5b71bb3c75ed5a045c913be3859
jeffrynasri <jeffryfaruki@gmail.com>
2017-03-09 04:14:07 +0700

    Konfigurasi Soal 3 SElesai

---
+++ Praktikum1/soal3/provisioning3.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y apache2
+apt-get update
+apt-get install -y python-software-properties
+add-apt-repository ppa:ondrej/php
+apt-get update
+apt-get install -y php7.0
